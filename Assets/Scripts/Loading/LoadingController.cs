c3409f9430ba9b8f703b5df3bc45a5c4dd8567b9
jvgoncalves935 <jvgoncalves935@hotmail.com>
2023-02-02 22:19:32 -0300

    Scripts Misc Visions

---
+++ Assets/Scripts/Loading/LoadingController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour
+{
+    private bool primeiroUpdate;
+    private AsyncOperation operacao;
+    [SerializeField] private Image imagemEH;
+
+    void Start()
+    {
+        primeiroUpdate = true;
+    }
+
+    private void Update() {
+        if(primeiroUpdate){
+            primeiroUpdate = false;
+            StartCoroutine(CarregarCena());
+            FocarMouse();
+        }
+        LogoPulsando();
+    }
+
+    private IEnumerator CarregarCena(){
+        string proximaCena = SceneLoader.InstanciaSceneLoader.GetProximaCena();
+        int indiceProximaCena = SceneLoader.InstanciaSceneLoader.GetIndiceProximaCena(proximaCena);
+
+        operacao = SceneManager.LoadSceneAsync(indiceProximaCena,LoadSceneMode.Single);
+        operacao.completed += (op) => PosLoading();
+        
+        //decimal progresso;
+
+        while(!operacao.isDone){
+            //progresso = (decimal) (Mathf.Clamp01(operacao.progress / 0.9f) * 100);
+            //textProgresso.text =  progresso.ToString("#.##")+ "%";
+            yield return null;
+        }
+    }
+
+    private void PosLoading(){
+        Debug.Log("pos loading");
+        AudioManager.InstanciaAudioManager.IniciarSonsCenaAtual();
+        SceneLoader.InstanciaSceneLoader.SetProximaCena("");
+        Debug.Log("acabou");
+    }
+
+    private void LogoPulsando(){
+        float valor = Mathf.Abs(0.5f + Mathf.Sin(Time.time * 2.0f)/2.0f);
+        //Debug.Log(valor);
+        imagemEH.color = new Color(1.0f, 1.0f, 1.0f, valor);
+    }
+
+    private void FocarMouse() {
+    #if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        MouseOperations.FocarMouseMultiPlat();
+    #endif
+    }
+}
---
+++ Assets/Scripts/Misc/SoundClip.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SoundClip : MonoBehaviour
+{
+    private int indice;
+    public string nome;
+    [SerializeField] private bool playOnStart;
+    [SerializeField] public bool efeitoSonoro;
+    [SerializeField] public bool wasPlaying;
+    [SerializeField] public bool ignorarPause;
+
+    private void Start(){
+        if(!efeitoSonoro) {
+            indice = AudioManager.InstanciaAudioManager.Find(nome);
+        }
+
+        //Debug.Log("nome: "+nome+", indice: "+indice);
+        if(playOnStart){
+            Play();
+        }
+    }
+
+    public void Play(){
+        AudioManager.InstanciaAudioManager.Play(nome);
+    }
+
+    public void Stop(){
+        AudioManager.InstanciaAudioManager.Stop(nome);
+    }
+
+    public void SetWasPlaying(bool flag) {
+        wasPlaying = flag;
+    }
+
+    public bool WasPlaying() {
+        return wasPlaying;
+    }
+
+    public void SetIgnorarPause(bool flag) {
+        ignorarPause = flag;
+    }
+
+    public bool IsIgnorarPause() {
+        return ignorarPause;
+    }
+
+}
