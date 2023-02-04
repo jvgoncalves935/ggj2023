d1bfeb3a59e5535c6cc6525276f964ce5f65c663
jvgoncalves935 <jvgoncalves935@hotmail.com>
2023-02-04 12:00:21 -0300

    Inicio Imagens Cutscene Floresta

---
+++ Assets/Scripts/Floresta/ImageCutsceneUIController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageCutsceneUIController : MonoBehaviour
+{
+    [SerializeField] private Image imagemUI;
+    [SerializeField] private List<Image> imagensCutscene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
